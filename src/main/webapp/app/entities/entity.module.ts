862b96914cec312235d7f5fde586ca8bc411ead1
Fakhrul Islam <fakhrulislam234@gmail.com>
2019-10-04 17:21:11 +0600

    route transaction module & add to entity menu

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     RouterModule.forChild([
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+      { path: 'entity/transactions', loadChildren: () => import('./transaction/transaction.module').then(m => m.AurumTransactionModule) }
     ])
   ]
 })
