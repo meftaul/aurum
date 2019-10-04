2d9d0838b49b06f9cd6a2b89a1810fd84a9fa1c2
Fakhrul Islam <fakhrulislam234@gmail.com>
2019-10-05 04:12:31 +0600

    transaction ui mockup

--- src/main/webapp/app/entities/transaction/components/transaction.component.ts
+++ src/main/webapp/app/entities/transaction/components/transaction.component.ts
@@ -1,11 +1,68 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Transaction } from './../services/domain/transaction.models';
+import { Customer } from './../services/domain/customer.models';
 
 @Component({
   selector: 'jhi-aurum-transaction',
-  templateUrl: './transaction.component.html'
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.scss']
 })
 export class TransactionComponent implements OnInit, OnDestroy {
-  ngOnInit() {}
+  customerID: string;
+  transactionForm: FormGroup;
+  customerForm: FormGroup;
+
+  displayedBillingInfoColumns: string[] = ['index', 'name', 'weight', 'symbol'];
+  billingInfos: any[] = [{ name: 'Hydrogen', weight: 1.0079, symbol: 'H' }];
+
+  itemNames: any[] = [
+    { value: 'Gold X-Ray', viewValue: 'Gold X-Ray' },
+    { value: '1 Gold Check (Mark)', viewValue: '1 Gold Check (Mark)' },
+    { value: 'Anklet', viewValue: 'Anklet' },
+    { value: 'Ayesti', viewValue: 'Ayesti' },
+    { value: 'Baju', viewValue: 'Baju' }
+  ];
+
+  serviceListColumns: string[] = ['index', 'itemName', 'quantity', 'weightGm', 'rate', 'amount', 'action'];
+  serviceList: any[] = [{ itemName: 'Anklet', quantity: 2, weightGm: 15, rate: 100, amount: 2000 }];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.prepareTransactionForm(new Transaction());
+    this.prepareCustomerForm(new Customer());
+  }
+
+  prepareTransactionForm(transactionData: Transaction) {
+    this.transactionForm = this.formBuilder.group({
+      voucherNo: [transactionData.voucherNo, [Validators.required]],
+      boxNo: [transactionData.boxNo, [Validators.required]],
+      karatType: [transactionData.karatType, [Validators.required]],
+      deliveryDate: [transactionData.deliveryDate, [Validators.required]]
+    });
+  }
+
+  prepareCustomerForm(customerData: Customer) {
+    this.customerForm = this.formBuilder.group({
+      name: [customerData.name, [Validators.required]],
+      mobileNumber: [customerData.mobileNumber, [Validators.required]],
+      address1: [customerData.address1, [Validators.required]],
+      address2: [customerData.address2]
+      // rewardPoints: [customerData.rewardPoints, [Validators.required]],
+    });
+  }
+
+  searchCustomer() {
+    /* eslint-disable no-console */
+    console.log(this.customerID);
+    /* eslint-enable no-console */
+  }
+
+  editCustomer() {}
+
+  saveCustomer() {}
 
   ngOnDestroy() {}
 }
--- src/main/webapp/app/entities/transaction/transaction.module.ts
+++ src/main/webapp/app/entities/transaction/transaction.module.ts
@@ -3,6 +3,13 @@ import { RouterModule } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AurumSharedModule } from 'app/shared/shared.module';
 
@@ -10,7 +17,20 @@ import { TransactionComponent } from './components/transaction.component';
 import { TRANSACTION_ROUTE } from './transaction.routes';
 
 @NgModule({
-  imports: [AurumSharedModule, RouterModule.forChild([TRANSACTION_ROUTE]), MatButtonModule, MatIconModule],
-  declarations: [TransactionComponent]
+  imports: [
+    AurumSharedModule,
+    RouterModule.forChild([TRANSACTION_ROUTE]),
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatSelectModule
+  ],
+  declarations: [TransactionComponent],
+  providers: [MatDatepickerModule]
 })
 export class AurumTransactionModule {}
