bcd2fc9025862ecf8ace9f00bfae52c84ba6ca5d
Fakhrul Islam Babar <fakhrulislam@mislbd.com>
2019-10-23 14:59:01 +0600

    Transaction route added

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'entity/transactions',
+        data: {
+          authorities: ['ROLE_ADMIN', 'ROLE_USER']
+        },
+        canActivate: [UserRouteAccessService],
+        loadChildren: () => import('./transaction/transaction.module').then(m => m.AurumTransactionModule)
+      },
       {
         path: 'customer',
         loadChildren: () => import('./customer/customer.module').then(m => m.AurumCustomerModule)
