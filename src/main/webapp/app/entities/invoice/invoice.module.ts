09f5d37d8b59ba78bfd910c12b77250e084a08ab
Fakhrul Islam <fakhrulislam234@gmail.com>
2019-12-09 08:54:33 +0600

    add invoice module

---
+++ src/main/webapp/app/entities/invoice/invoice.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { AurumSharedModule } from 'app/shared/shared.module';
+import { InvoiceComponent } from './components/invoice.component';
+import { INVOICE_ROUTE } from './invoice.routes';
+
+@NgModule({
+  imports: [
+    AurumSharedModule,
+    RouterModule.forChild(INVOICE_ROUTE),
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatSelectModule,
+    MatCheckboxModule
+  ],
+  declarations: [InvoiceComponent],
+  providers: [MatDatepickerModule]
+})
+export class AurumInvoiceModule {}
