587b75a4efd795c07be5ceb6733faab02f8c5a15
Fakhrul Islam Babar <fakhrulislam@mislbd.com>
2019-12-24 13:18:26 +0600

    rate script

---
+++ script.sql
@@ -0,0 +1,8 @@
+INSERT INTO RATE (id, rate_type, unit_price)
+    VALUES (1, 'X-Ray', 100),
+           (2, 'Hallmark', 50),
+           (3, 'Normal Melting', 150),
+           (4, 'Calculated Melting', 150),
+           (5, 'AU', 50),
+           (6, 'SI', 4),
+           (7, 'CU', 20);
