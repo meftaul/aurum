098831f5ab6e46f542f04cef92be45f4350f2710
meftaul <meftaulhaque@gmail.com>
2019-12-28 03:50:28 +0600

    Messaging service is added

--- src/main/webapp/app/entities/messaging/components/messaging.component.ts
+++ src/main/webapp/app/entities/messaging/components/messaging.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MessageService } from 'app/entities/messaging/services/messaging.service';
 
 @Component({
   selector: 'jhi-aurum-messaging',
@@ -6,9 +7,23 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./messaging.component.scss']
 })
 export class MessagingComponent implements OnInit, OnDestroy {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   ngOnInit() {}
 
   ngOnDestroy() {}
+
+  sendMessage() {
+    // eslint-disable-next-line no-console
+    console.info('sending message');
+    this.messageService.postMessage('01723466685', 'Hello from MaitreeGold').subscribe(
+      data => {
+        // eslint-disable-next-line no-console
+        console.log(data);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
---
+++ src/main/webapp/app/entities/messaging/model/message.model.ts
@@ -0,0 +1,6 @@
+export class MessageModel {
+  username: string;
+  password: string;
+  number: any;
+  message: string;
+}
