7c34bad487c5b3045806efb60c9dc34af8bba748
Fakhrul Islam Babar <fakhrulislam@mislbd.com>
2020-07-26 18:31:33 +0600

    charge for extra weight set to 11 taka

--- src/main/webapp/app/entities/transaction/components/transaction.component.ts
+++ src/main/webapp/app/entities/transaction/components/transaction.component.ts
@@ -651,7 +651,7 @@ export class TransactionComponent implements OnInit, OnDestroy {
         this.selectedServiceCharge = this.rateTypePriceMap.get(serviceTypeTemp);
         this.aurumServiceForm.controls.rate.setValue(this.selectedServiceCharge);
       } else if (serviceTypeTemp === 'Normal Melting' || serviceTypeTemp === 'Calculated Melting') {
-        const extraWeightToCharge = +(gramValue - 116).toFixed(2);
+        const extraWeightToCharge: number = +(+(gramValue - 116).toFixed(2) * 11).toFixed(2);
         this.selectedServiceCharge = this.rateTypePriceMap.get(serviceTypeTemp) + extraWeightToCharge;
         this.aurumServiceForm.controls.rate.setValue(this.selectedServiceCharge);
       }
